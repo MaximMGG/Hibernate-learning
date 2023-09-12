bb069b8e571602a7e97f5e8a70db0c1ee2539cd1
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-12 13:19:01 +0300

    create entity and persist in db and more

--- src/main/java/com/maxim/hibernate/HibernateRunner.java
+++ src/main/java/com/maxim/hibernate/HibernateRunner.java
@@ -1,10 +1,13 @@
 package com.maxim.hibernate;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import com.maxim.hibernate.entity.User;
 
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
@@ -13,11 +16,24 @@ public class HibernateRunner {
         // Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "pass");
         // Session
         Configuration configuration = new Configuration();
+        // configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        // configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                            .username("Poko123")
+                            .firstname("Pok")
+                            .lastname("Mok")
+                            .birthDate(LocalDate.of(1999, 11, 1))
+                            .age(14)
+                            .build();
+
+            session.persist(user);
+            session.getTransaction().commit();
         }
         
     }
---
+++ src/main/java/com/maxim/hibernate/entity/User.java
@@ -0,0 +1,30 @@
+package com.maxim.hibernate.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer age;
+
+}
