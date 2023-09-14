2dfd5218d3b7b35f2cdb535301abcf0f0762c03a
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-14 18:10:40 +0300

    logging example, but not work yet\

--- src/main/java/com/maxim/hibernate/HibernateRunner.java
+++ src/main/java/com/maxim/hibernate/HibernateRunner.java
@@ -6,37 +6,39 @@ import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.maxim.hibernate.entity.User;
 import com.maxim.hibernate.util.HibernateUtils;
 
 public class HibernateRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(HibernateRunner.class);
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
                         .username("Piter@gmail.com")
                         .lastname("Bobson")
                         .firstname("Piter")
                         .build();
+        logger.info("user entity is in transient state, object: {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtils.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+                Transaction transaction = session1.beginTransaction();
+                logger.trace("Transaction was create, {}", transaction);
 
                 session1.merge(user);
+                logger.trace("Use in persistent state, {}, {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setFirstname("Paul");
-                // session2.remove(user);
-
-                session2.refresh(user);
-
-                session2.getTransaction().commit();
-            }
+            logger.warn("User is in detached state: {}, session is closed {}", user, session1);
+        } catch (Exception e) {
+            logger.error("Exception occurred", e);
+            throw e;
         }
         
     }
