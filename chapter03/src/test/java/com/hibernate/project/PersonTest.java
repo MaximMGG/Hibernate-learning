2e9c701ee81e7f7ce31ea07b37c0296ee2866ceb
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-08 16:11:02 +0300

    one more method for testRanking

--- chapter03/src/test/java/com/hibernate/project/PersonTest.java
+++ chapter03/src/test/java/com/hibernate/project/PersonTest.java
@@ -1,5 +1,8 @@
 package com.hibernate.project;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,6 +10,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -86,5 +90,60 @@ public class PersonTest {
             session.save(person);
         }
         return person;
+    }
+
+    @Test
+    public void testRankings() {
+        populateRankingData();
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            Query<Ranking> query = session.createQuery("from Ranking r "
+                                            + "where r.subject.name=:name "
+                                            + "and r.skill.name=:skill", Ranking.class);
+            query.setParameter("name", "J. C. Smell");
+            query.setParameter("skill", "Java");
+
+            IntSummaryStatistics stats = query.list()
+                                                .stream()
+                                                .collect(Collectors.summarizingInt(Ranking::getRanking));
+
+            long count = stats.getCount();
+            int average = (int) stats.getAverage();
+            
+            tx.commit();
+
+            session.close();
+            Assert.assertEquals(count, 3);
+            Assert.assertEquals(average, 7);
+        }
+    }
+
+
+    private void populateRankingData() {
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            createDate(session, "J. C. Smell", "Gene Showrama", "Java", 6);
+            createDate(session, "J. C. Smell", "Scottaball Most", "Java", 7);
+            createDate(session, "J. C. Smell", "Drew Lombardo", "Java", 8);
+
+            tx.commit();
+        }
+    }
+
+
+    private void createDate(Session session, String subjectName, String observerName, String skillName, int rank) {
+        Person subject = savePerson(session, subjectName);
+        Person observer = savePerson(session, observerName);
+        Skill skill = saveSkill(session, skillName);
+
+        Ranking ranking = new Ranking();
+        ranking.setSubject(subject);
+        ranking.setObserver(observer);
+        ranking.setSkill(skill);
+        ranking.setRanking(rank);
+
+        session.save(ranking);
     }
 }
