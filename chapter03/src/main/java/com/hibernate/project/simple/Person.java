44895d13c745beab3e321e42a3b7c602f84fec57
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-08 14:38:56 +0300

    init chapter03

---
+++ chapter03/src/main/java/com/hibernate/project/simple/Person.java
@@ -0,0 +1,15 @@
+package com.hibernate.project.simple;
+
+public class Person {
+    
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
